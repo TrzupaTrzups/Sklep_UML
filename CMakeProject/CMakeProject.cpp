11aaed48989b7c673b33c21929b9e267e16b488e
Piotr Porębski <piopor134@gmail.com>
2026-06-10 08:20:05 +0200

    Zaczęto implementacje w c++

---
+++ CMakeProject/CMakeProject.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "CMakeProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
