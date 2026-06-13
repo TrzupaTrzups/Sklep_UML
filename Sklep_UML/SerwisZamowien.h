3ba118db74b0911e22f66550225d4e04fddc6aa5
MR20203 <119396706+MR20203@users.noreply.github.com>
2026-06-13 18:54:14 +0200

    Implementacja SerwisZamowien, klasa AdresDostawy, naprawa Zamowienie
    - AdresDostawy: nowa klasa (odbiorca/ulica/kod/miasto/telefon + czyKompletny, wyswietl)
    - SerwisZamowien: utworzZamowienie/zmienStatus/pobierzZamowieniePoId/pobierzZamowieniaKlienta (w pamieci)
    - Zamowienie: naprawione metody zmienStatus/wyswietlPodsumowanie/wyswietlSzczegoly

---
+++ Sklep_UML/SerwisZamowien.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "Zamowienie.h"
+#include "Koszyk.h"
+#include "AdresDostawy.h"
+
+#include <vector>
+
+using namespace std;
+
+class SerwisZamowien {
+private:
+    vector<Zamowienie> zamowienia;
+    int nastepneId = 1;
+
+public:
+    // Na diagramie return typy serwisu sa niespojne (np. "vector"/"boolean" tam,
+    // gdzie logicznie powinien byc pojedynczy obiekt). Nazwy i parametry sa wg
+    // diagramu; typy zwracane dobrane logicznie.
+
+    // Tworzy zamowienie z koszyka i adresu. Zwraca true, gdy sie udalo
+    // (koszyk niepusty i adres kompletny). Nadaje kolejne unikalne ID.
+    bool utworzZamowienie(int idKlienta, Koszyk& koszyk, AdresDostawy& adres) {
+        if (koszyk.czyPusty()) {
+            return false;
+        }
+
+        if (!adres.czyKompletny()) {
+            return false;
+        }
+
+        double wartosc = koszyk.obliczWartosc();
+
+        Zamowienie zamowienie(
+            nastepneId,
+            idKlienta,
+            koszyk,
+            wartosc,
+            adres,
+            StatusZamowienia::Utworzone
+        );
+
+        zamowienia.push_back(zamowienie);
+        nastepneId++;
+
+        return true;
+    }
+
+    bool zmienStatus(int idZamowienia, StatusZamowienia nowyStatus) {
+        for (Zamowienie& zamowienie : zamowienia) {
+            if (zamowienie.id == idZamowienia) {
+                return zamowienie.zmienStatus(nowyStatus);
+            }
+        }
+
+        return false;
+    }
+
+    // Zwraca wskaznik do zamowienia o danym ID, lub nullptr jesli nie istnieje
+    // (spojnie z SerwisProduktow::znajdzProduktPoId).
+    Zamowienie* pobierzZamowieniePoId(int idZamowienia) {
+        for (Zamowienie& zamowienie : zamowienia) {
+            if (zamowienie.id == idZamowienia) {
+                return &zamowienie;
+            }
+        }
+
+        return nullptr;
+    }
+
+    vector<Zamowienie> pobierzZamowieniaKlienta(int idKlienta) {
+        vector<Zamowienie> wynik;
+
+        for (Zamowienie& zamowienie : zamowienia) {
+            if (zamowienie.idKlienta == idKlienta) {
+                wynik.push_back(zamowienie);
+            }
+        }
+
+        return wynik;
+    }
+
+    const vector<Zamowienie>& pobierzWszystkieZamowienia() const {
+        return zamowienia;
+    }
+};
